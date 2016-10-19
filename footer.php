91d0de9067160260aa75e68ece5305b261bd7ea0
Irene Folkestad Sønsteby <turidfs@hiof.no>
2016-10-19 15:21:03 +0200

    Oppdatert med php filer.

---
+++ footer.php
@@ -0,0 +1,15 @@
+	<footer>
+        <section id="social">
+			<ul>
+				<li><a href="http://www.twitter.com">Twitter</a></li>
+				<li><a href="http://www.facebook.com">Facebook</a></li>
+				<li><a href="http://www.youtube.com">Youtube</a></li>
+				<li><a href="http://www.soundcloud.com">Soundcloud</a></li>
+			</ul>
+		</section>
+		<section id="copyrights">
+			<p>© 2016 Zelda In Park. All rights reserved. Design by Awesome People. Powered by Hiof</p>
+			<p><a href="">Privacy Policy</a> | <a href="">Terms of Service</a> | <a href="">WU Privacy Policy</a> | <a href="">WU Terms of Service</a></p>
+			<p>Visit <a href="">www.OnGuardOnline.gov</a> for social networking safety tips for parents and youth.</p>
+		</section>
+	</footer>
---
+++ header.php
@@ -0,0 +1,45 @@
+
+	<header>
+        <!-- <h1><a href="index.html">ZiP</a></h1> -->
+        <a href="index.html">
+            <svg height="100" width="150" class="logo">
+            <defs>
+                <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+                    <stop offset="0%" style="stop-color:rgb(255,255,0);stop-opacity:1" />
+                    <stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+                </linearGradient>
+            </defs>
+            <ellipse cx="60" cy="50" rx="60" ry="50" fill="url(#grad1)" />
+            <text fill="#ffffff" font-size="45" font-family="Verdana" x="25" y="60">ZiP</text>
+            Sorry, your browser does not support inline SVG.
+            </svg>
+        </a>
+        <svg width="100" height="100" class="menu">
+            <circle cx="50" cy="50" r="50" fill="yellow" />
+            <rect x="25" y="20" rx="5" ry="10" width="50" height="10" style="fill:rgb(0,0,255);" />
+            <rect x="25" y="45" rx="5" ry="10" width="50" height="10" style="fill:rgb(0,0,255);" />
+            <rect x="25" y="70" rx="5" ry="10" width="50" height="10" style="fill:rgb(0,0,255);" />
+        </svg>
+        
+        <nav>
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="about.html">About</a></li>
+                <li><a href="events.html">Events/Tours</a></li>
+                <li><a href="social.html">Social</a>
+                    <ul>
+                        <li><a href="blog.html">Blog</a></li>
+                    </ul>
+                </li>
+				<li><a href="media/tracks.html">Media</a>
+					<ul>
+						<li><a href="media/tracks.html">Tracks</a></li>
+						<li><a href="media/pictures.html">Pictures</a></li>
+						<li><a href="media/videos.html">Videos</a></li>
+					</ul>
+				</li>
+                <li><a>Shop</a></li>
+				
+			</ul>
+		</nav>
+	</header>
